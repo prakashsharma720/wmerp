f816cea58d812daddd4a15857cb6a4f25e85dc2d
developerAnsh02 <quentumnodeinnovation@gmail.com>
2025-07-12 22:58:18 +0530

    update new layout in layout folder

---
+++ application/views/layout/css.php
@@ -0,0 +1,9 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="<?php echo base_url()."assets2/"; ?>/css/bootstrap.min.css" type="text/css" />
+
+<!-- Vendor CSS -->
+<link rel="stylesheet" href="<?php echo base_url()."assets2/"; ?>/vendors/css/vendors.min.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo base_url()."assets2/"; ?>/vendors/css/daterangepicker.min.css" type="text/css" />
+
+<!-- Theme / Custom CSS -->
+<link rel="stylesheet" href="<?php echo base_url()."assets2/"; ?>/css/theme.min.css" type="text/css" />
