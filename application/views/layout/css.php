a1e7b16a3461ae5e90ac39d8136f9f90fcaff7f1
developerAnsh02 <quentumnodeinnovation@gmail.com>
2025-07-14 17:35:21 +0530

    Lead Generation Module
    Update All Ui Change in lead Generation index file Only Edit , update And Bulk Select Is pading

---
+++ application/views/layout/css.php
@@ -0,0 +1,23 @@
+<!-- BEGIN: Favicon -->
+<link rel="shortcut icon" type="image/x-icon" href="<?= base_url('assets2/images/favicon.ico'); ?>">
+<!-- END: Favicon -->
+
+<!-- BEGIN: Bootstrap CSS -->
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets2/css/bootstrap.min.css'); ?>">
+<!-- END: Bootstrap CSS -->
+
+<!-- BEGIN: Vendors CSS -->
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets2/vendors/css/vendors.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets2/vendors/css/dataTables.bs5.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets2/vendors/css/tagify.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets2/vendors/css/tagify-data.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets2/vendors/css/quill.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets2/vendors/css/select2.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets2/vendors/css/select2-theme.min.css'); ?>">
+<!-- END: Vendors CSS -->
+
+<!-- BEGIN: Custom Theme CSS -->
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets2/css/theme.min.css'); ?>">
+<!-- END: Custom Theme CSS -->
+<link rel="stylesheet" href="<?= base_url('assets2/vendors/css/daterangepicker.min.css'); ?>" type="text/css" />
+<link rel="stylesheet" href="<?= base_url('assets2/vendors/css/datepicker.min.css'); ?>" type="text/css" />
