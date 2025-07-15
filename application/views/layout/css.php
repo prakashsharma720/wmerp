2a9779a016960990c65f6e604672798dde5b0e1c
prakashsharma720 <prakashsharma720@gmail.com>
2025-07-15 12:21:07 +0530

    updated

--- application/views/layout/css.php
+++ application/views/layout/css.php
@@ -1,9 +1,12 @@
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="<?php echo base_url()."assets2/"; ?>/css/bootstrap.min.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo base_url()."assets2/"; ?>css/bootstrap.min.css" type="text/css" />
 
 <!-- Vendor CSS -->
-<link rel="stylesheet" href="<?php echo base_url()."assets2/"; ?>/vendors/css/vendors.min.css" type="text/css" />
-<link rel="stylesheet" href="<?php echo base_url()."assets2/"; ?>/vendors/css/daterangepicker.min.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo base_url()."assets2/"; ?>vendors/css/vendors.min.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo base_url()."assets2/"; ?>vendors/css/daterangepicker.min.css" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()."assets2/"; ?>vendors/css/vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()."assets2/"; ?>vendors/css/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()."assets2/"; ?>vendors/css/select2-theme.min.css">
 
 <!-- Theme / Custom CSS -->
-<link rel="stylesheet" href="<?php echo base_url()."assets2/"; ?>/css/theme.min.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo base_url()."assets2/"; ?>css/theme.min.css" type="text/css" />
--- application/views/layout/js.php
+++ application/views/layout/js.php
@@ -1,15 +1,19 @@
 <!-- BEGIN: Core Vendor Scripts -->
-<script src="<?php echo base_url()."assets2/"; ?>/vendors/js/vendors.min.js"></script> <!-- Required core dependencies -->
-<script src="<?php echo base_url()."assets2/"; ?>/vendors/js/daterangepicker.min.js"></script>
-<script src="<?php echo base_url()."assets2/"; ?>/vendors/js/apexcharts.min.js"></script>
-<script src="<?php echo base_url()."assets2/"; ?>/vendors/js/circle-progress.min.js"></script>
+<script src="<?php echo base_url()."assets2/"; ?>vendors/js/vendors.min.js"></script> <!-- Required core dependencies -->
+<script src="<?php echo base_url()."assets2/"; ?>vendors/js/daterangepicker.min.js"></script>
+<script src="<?php echo base_url()."assets2/"; ?>vendors/js/select2.min.js"></script>
+<script src="<?php echo base_url()."assets2/"; ?>vendors/js/select2-active.min.js"></script>
+<script src="<?php echo base_url()."assets2/"; ?>vendors/js/apexcharts.min.js"></script>
+<script src="<?php echo base_url()."assets2/"; ?>vendors/js/circle-progress.min.js"></script>
 <!-- END: Core Vendor Scripts -->
 
 <!-- BEGIN: Application Initialization -->
-<script src="<?php echo base_url()."assets2/"; ?>/js/common-init.min.js"></script> <!-- General app setup -->
-<script src="<?php echo base_url()."assets2/"; ?>/js/dashboard-init.min.js"></script> <!-- Dashboard-specific logic -->
+<script src="<?php echo base_url()."assets2/"; ?>js/common-init.min.js"></script> <!-- General app setup -->
+<script src="<?php echo base_url()."assets2/"; ?>js/customers-view-init.min.js"></script>
+
+<script src="<?php echo base_url()."assets2/"; ?>js/dashboard-init.min.js"></script> <!-- Dashboard-specific logic -->
 <!-- END: Application Initialization -->
 
 <!-- BEGIN: Theme Customizer -->
-<script src="<?php echo base_url()."assets2/"; ?>/js/theme-customizer-init.min.js"></script>
+<script src="<?php echo base_url()."assets2/"; ?>js/theme-customizer-init.min.js"></script>
 <!-- END: Theme Customizer -->
